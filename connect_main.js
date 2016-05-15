9fe7cc727a0364a4f5a6561b753d8a1dfcecd13d
njhal7 <njhal7@student.monash.edu>
2016-05-15 22:15:17 +1000

    Basic strain values and id into sqlite3

--- connect_main.js
+++ connect_main.js
@@ -1,12 +1,40 @@
+// Libraries
 var noble = require("noble");
 var fs = require("fs");
+var sqlite3 = require("sqlite3").verbose();
 
+// Constants
 const PERIPHERAL_NAME = "Train";
 const SENSOR_SERVICE_UUID = "0000000000001000800000805f9b34f0";
 const ACCELE_CH_UUID = "0000000000001000800000805f9b34f1";
 const STRAIN_CH_UUID = "0000000000001000800000805f9b34f2";
 
+// Globals
 var peripherals = [];
+var dbNodes;
+
+
+function initDb() {
+	// Open file
+	var dbFile = "nodes.db";
+	var dbExists = fs.existsSync(dbFile);
+
+	if (!dbExists) {
+		fs.openSync(dbFile, "w");
+	}
+	
+	// Create DB handler
+	dbNodes = new sqlite3.Database(dbFile);
+	
+	dbNodes.serialize(function() {
+		// Create table
+		if (!dbExists) {
+			console.log("Creating sqlite3 DB table...");
+			dbNodes.run("CREATE TABLE strains (node_id TEXT, value TEXT)");
+			console.log("Created sqlite3 DB table");
+		}
+	});
+}
 
 
 noble.on("stateChange", function(state) {
@@ -41,17 +69,18 @@ noble.on("discover", function(peripheral) {
 					});
 					
 					// Create file for node
-					peripheral.logFile = fs.createWriteStream("node_" + peripheral.id + ".log", { flags: "a" });
+					//peripheral.logFile = fs.createWriteStream("node_" + peripheral.id + ".log", { flags: "a" });
+					
+					// Prepare SQL stmt
+					peripheral.strainStmt = dbNodes.prepare("INSERT INTO strains VALUES (?, ?)");
 					
 					// Notify ch
 					if (peripheral.strainCh != null) {
 						peripheral.strainCh.on("data", function(data, isNotification) {
 							//console.log(peripheral.id + ": " + data.readUInt16BE(0));
-							//console.log(data.readUInt16BE(0) + ",");
-							peripheral.logFile.write(data.readUInt16BE(0) + ",");
+							//peripheral.logFile.write(data.readUInt16BE(0) + ",");
+							peripheral.strainStmt.run(peripheral.id, data.readUInt16BE(0));
 						});
-						
-						//peripheral.strainCh.notify(true);
 					}
 				});
 			});
@@ -64,12 +93,23 @@ noble.on("discover", function(peripheral) {
 var exitHandler = function exitHandler() {
 	console.log("\nPreparing to exit...");
 	
+	// Close all peripheral connections
     peripherals.forEach(function(peripheral) {
         console.log("Disconnecting from " + peripheral.uuid + "...");
+        
+        // End BLE connection
         peripheral.disconnect( function(){
 			console.log("Disconnected from " + peripheral.uuid);
+			
+			// Finalise SQL statements
+			peripheral.strainStmt.finalize();
         });
     });
+    
+    // Close DB
+    console.log("\nClosing sqlite3 DB...");
+	dbNodes.close();
+	console.log("Closed sqlite3 DB");
 
     // End process after 1.5 more seconds
     setTimeout(function(){
@@ -78,7 +118,11 @@ var exitHandler = function exitHandler() {
 }
 
 function main() {
+	// Enable exit handler
 	process.on("SIGINT", exitHandler);
+	
+	// Init DB
+	initDb();
 	
 	// Wait before turning on notifys
 	setTimeout(function(){
