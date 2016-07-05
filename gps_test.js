fe886f3ce375b76f8ff64fe4b62252d952e885a3
njhal7 <njhal7@student.monash.edu>
2016-07-05 19:10:13 +1000

    Script to test GPS. Prints lat, lon

---
+++ gps_test.js
@@ -0,0 +1,14 @@
+// Libraries
+var gpsd = require("node-gpsd");
+
+// Globals
+var gpsdListener = new gpsd.Listener();
+
+gpsdListener.on("TPV", function (data) {
+	//console.log(data);
+	console.log(data.lat + ", " + data.lon);
+});
+
+gpsdListener.connect(function() {
+	gpsdListener.watch({ class: "WATCH", json: true, nmea: false });
+});
