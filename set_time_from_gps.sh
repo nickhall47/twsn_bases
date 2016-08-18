a01459ac0154c2deadd13a4b9504924aa40c5374
njhal7 <njhal7@student.monash.edu>
2016-08-18 20:00:23 +1000

    Added set time from gps script (run on boot)

---
+++ set_time_from_gps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pkill ntpd
+pkill gpsd
+gpsd -b -n -D 2 /dev/ttyUSB0
+sleep 4
+GPSDATE=`gpspipe -w | head -10 | grep TPV | sed -r 's/.*"time":"([^"]*)".*/\1/' | head -1`
+echo $GPSDATE
+date -s "$GPSDATE"
+/usr/sbin/ntpd
