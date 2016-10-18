fcf4300c888af99bf296d586745bfbe10ecd15c4
nick_hall47 <nick_hall47@hotmail.com>
2016-10-18 20:27:35 +1100

    Updated noble library

--- connect_main.js
+++ connect_main.js
@@ -15,7 +15,7 @@ var EventDataPoint = require("./EventDataPoint");
 // Flags
 const EVENT_DETECTION_ENABLED_FLAG = 0;
 const AUTO_SHUTDOWN_TIMEOUT_FLAG = 0;
-const MAX_NUM_NODES = 6; // Optional (Set to 0 to have no max)
+const MAX_NUM_NODES = 1; // Optional (Set to 0 to have no max)
 const MAX_DATA_BEFORE_INSERT = MAX_NUM_NODES*600; // ~60 secs worth of data in standard config
 const MAX_DATA_BEFORE_INSERT_EVENTS = MAX_NUM_NODES*60;
 
